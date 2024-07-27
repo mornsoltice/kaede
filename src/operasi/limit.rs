780e7f6202bdd0a7bce82ee842996817bcfb74d4
Ken Toad <khairandramnandyka@gmail.com>
2024-07-27 08:59:49 +0700

    updating limit thing

--- src/operasi/limit.rs
+++ src/operasi/limit.rs
@@ -6,11 +6,22 @@ where
 {
     let limit_left = f(x - epsilon);
     let limit_right = f(x + epsilon);
-    let tolerance = 1e-6; 
-    
+
+    let tolerance = 1e-6;
+
+    let value_tolerance = 1e-6;
+
     if (limit_left - limit_right).abs() < tolerance {
         Ok((limit_left + limit_right) / 2.0)
     } else {
-        Err(MathError::TipeError("Limit tidak terdefinisi".to_string()))
+        let average_limit = (limit_left + limit_right) / 2.0;
+        let left_stable = (limit_left - average_limit).abs() < value_tolerance;
+        let right_stable = (limit_right - average_limit).abs() < value_tolerance;
+
+        if left_stable && right_stable {
+            Ok(average_limit)
+        } else {
+            Err(MathError::TipeError("Limit tidak terdefinisi".to_string()))
+        }
     }
 }
