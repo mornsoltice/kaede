50ffa430beef5a304c7943f7722d6114f166333d
Ken Toad <khairandramnandyka@gmail.com>
2024-07-27 09:08:01 +0700

    debugging limit fix shit rah

--- src/operasi/limit.rs
+++ src/operasi/limit.rs
@@ -6,22 +6,15 @@ where
 {
     let limit_left = f(x - epsilon);
     let limit_right = f(x + epsilon);
-
     let tolerance = 1e-6;
 
-    let value_tolerance = 1e-6;
+    println!("Limit left: {}", limit_left);
+    println!("Limit right: {}", limit_right);
+    println!("Tolerance: {}", tolerance);
 
     if (limit_left - limit_right).abs() < tolerance {
         Ok((limit_left + limit_right) / 2.0)
     } else {
-        let average_limit = (limit_left + limit_right) / 2.0;
-        let left_stable = (limit_left - average_limit).abs() < value_tolerance;
-        let right_stable = (limit_right - average_limit).abs() < value_tolerance;
-
-        if left_stable && right_stable {
-            Ok(average_limit)
-        } else {
-            Err(MathError::TipeError("Limit tidak terdefinisi".to_string()))
-        }
+        Err(MathError::TipeError("Limit tidak terdefinisi".to_string()))
     }
 }
